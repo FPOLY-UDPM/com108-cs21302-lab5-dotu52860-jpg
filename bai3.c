c8fbc9b9830bf670bf4be3c026f6430211fed5fb
dotu52860-jpg <dotu52860@gmail.com>
2026-02-03 10:45:24 +0700

    nộp lab

--- bai3.c
+++ bai3.c
@@ -1,7 +1,7 @@
 /******************************************************************************
- * Họ và tên: [ĐIỀN TÊN TẠI ĐÂY]
- * MSSV:      [ĐIỀN MSSV TẠI ĐÂY]
- * Lớp:       [ĐIỀN LỚP TẠI ĐÂY]
+ * Họ và tên: [Đỗ Minh Tú]
+ * MSSV:      [PS 48536]
+ * Lớp:       [COM108-CS21302]
  *****************************************************************************/
 
 //  BÀI 3: XÂY DỰNG HÀM HOÁN VỊ 
@@ -12,12 +12,29 @@
 
 #include <stdio.h>
 
-//Tạo hàm 
+// Tạo hàm hoán vị sử dụng con trỏ
+void hoanVi(int *a, int *b) {
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
 
 int main() {
+    int a, b;
 
-    // //Gọi hàm trong hàm main 
-    
+    // Nhập 2 giá trị
+    printf("Nhap a va b: ");
+    scanf("%d %d", &a, &b);
+
+    // Gọi hàm hoán vị
+    hoanVi(&a, &b);
+
+    // In kết quả sau khi hoán vị
+    printf("Sau khi hoan vi:\n");
+    printf("a = %d\n", a);
+    printf("b = %d\n", b);
+
     return 0;
 }
 
